deb9b8b6fdbfe2ba40f29f4ca670198f2ff50e7f
Timur <puzino000@gmail.com>
2022-03-17 11:05:35 +0200

    refactoring and fix flake8

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,19 +1,3 @@
-"""settings URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from currency import views as currency_views
 
 from django.contrib import admin
@@ -22,7 +6,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world/', currency_views.hello_world),
+    path('', currency_views.hello_world),
     path('contact_us/list/', currency_views.contact_us_list),
     path('rate/list/', currency_views.rate_list),
 
