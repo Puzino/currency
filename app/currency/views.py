d18482a96bc54e46c44eac2ff52fe473b5652d23
Timur <puzino000@gmail.com>
2022-03-18 09:22:29 +0200

    add model Source, add CRUD for Source list

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
 
-from .models import ContactUs, Rate
+from .forms import SourceForm
+from .models import ContactUs, Rate, Source
 
 
 def hello_world(request):
@@ -8,10 +9,53 @@ def hello_world(request):
 
 
 def contact_us_list(request):
-    rates = ContactUs.objects.all()
-    return render(request, 'contactus_list.html', {'rates': rates})
+    rates = ContactUs.objects.all().order_by('-id')
+    return render(request, 'contactus_list.html', context={'rates': rates})
 
 
 def rate_list(request):
-    rates = Rate.objects.all()
-    return render(request, 'rate_list.html', {'rates': rates})
+    rates = Rate.objects.all().order_by('-id')
+    return render(request, 'rate_list.html', context={'rates': rates})
+
+
+def source_list(request):
+    source = Source.objects.all().order_by('-id')
+    return render(request, 'source_list.html', context={'source': source})
+
+
+def source_create(request):
+    if request.method == "POST":
+        form = SourceForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return HttpResponseRedirect('/source/list/')
+    else:
+        form = SourceForm()
+    return render(request, 'source_create.html', {'form': form})
+
+
+def source_detail(request, pk):
+    source = get_object_or_404(Source, pk=pk)
+    return render(request, 'source_detail.html', context={'source': source})
+
+
+def source_edit(request, pk):
+    source = get_object_or_404(Source, pk=pk)
+    if request.method == "POST":
+        form = SourceForm(request.POST, instance=source)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return HttpResponseRedirect('/source/list/')
+    else:
+        form = SourceForm(instance=source)
+    return render(request, 'source_edit.html', context={'form': form})
+
+
+def source_delete(request, pk):
+    source = get_object_or_404(Source, pk=pk)
+    if request.method == 'POST':
+        source.delete()
+        return HttpResponseRedirect('/source/list/')
+    return render(request, 'source_delete.html', context={'source': source})
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,13 +1,16 @@
-from currency import views as currency_views
-
 from django.contrib import admin
 from django.urls import path
 
+from currency import views as currency_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', currency_views.hello_world),
     path('contact_us/list/', currency_views.contact_us_list),
     path('rate/list/', currency_views.rate_list),
-
+    path('source/list/', currency_views.source_list),
+    path('source/create/', currency_views.source_create),
+    path('source/detail/<int:pk>/', currency_views.source_detail),
+    path('source/delete/<int:pk>/', currency_views.source_delete),
+    path('source/edit/<int:pk>/', currency_views.source_edit),
 ]
